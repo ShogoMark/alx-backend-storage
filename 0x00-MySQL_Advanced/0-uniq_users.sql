5824b179270fe7204c5493dff5f6f1ab75a525d2
ShogoMark <trigmark87@gmail.com>
2023-07-14 15:04:32 +0100

    task0

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,7 @@
 -- creating a table with attributes
 
 CREATE TABLE users IF NOT EXISTS (
-	id INT NOT NULL AUTO INCREMENT PRIMARY KEY,
+	id INT NOT NULL AUTO INCREMENT,
 	email VARCHAR(255) NOT NULL UNIQUE,
 	name VARCHAR(255)
 );
