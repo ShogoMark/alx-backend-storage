55416290b8f50b896ebc0b300196510531f54fb1
ShogoMark <trigmark87@gmail.com>
2023-07-19 14:15:47 +0100

    task0

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+"""a class Cache and method store that takes in data argument"""
 
 import redis
 import uuid
@@ -11,6 +12,7 @@ class Cache:
         self._redis.flushdb()
 
     def store(self, data) -> str:
+        """takes in data as argument and return key as strings"""
         key = str(uuid.uuid4())
         self._redis.setex(key, 3600, data)
 
